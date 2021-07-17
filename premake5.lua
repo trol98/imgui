1a62172af5b46a47afd1b7264bb71ac5ad500b15
trol98 <seweryn-szymon@outlook.com>
2021-07-17 11:29:58 +0200

    Reconfig

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,13 @@
+IncludeDir = {}
+IncludeDir["GLFW"] = "%{wks.location}/Dependencies/GLFW/include"
+IncludeDir["Glad"] = "%{wks.location}/Dependencies/Glad/include"
+
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
 	systemversion "latest"
 	cppdialect "C++17"
-	staticruntime "off"
+	staticruntime "On"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,7 +24,22 @@ project "ImGui"
 		"imstb_textedit.h",
 		"imstb_truetype.h",
 		"imgui_demo.cpp",
-		"imgui_tables.cpp"
+        "imgui_tables.cpp",
+        "imgui_impl_glfw.h",
+        "imgui_impl_glfw.cpp",
+        "imgui_impl_opengl3.h",
+        "imgui_impl_opengl3.cpp"
+	}
+
+    defines
+	{
+		"IMGUI_IMPL_OPENGL_LOADER_GLAD"
+	}
+
+    includedirs
+	{
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}"
 	}
 
 	filter "configurations:Debug"
